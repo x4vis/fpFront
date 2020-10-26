9d20f8e81033c79c5f7f366d9b5e364b21ece7ef
Javier Soto <x4v15s@MacBook-Pro-de-Javier.local>
2020-10-25 20:48:14 -0600

    se agrega btn de crear usuario, considerar crear componente de creacion de recurso

--- src/views/Admin/Usuarios/ListarUsuarios.tsx
+++ src/views/Admin/Usuarios/ListarUsuarios.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Button } from 'antd';
+import { Link } from "react-router-dom";
 
 const ListarUsuarios = () => {
     return (
         <div className="container-fluid">
             <h1 className="blue bw-text biggest-text">Usuarios</h1>
+
+            <div className="row">
+                <div className="col-6">
+                    <Button className="blue-back white fullWidth"
+                            type="primary"
+                            size="large">
+                        <Link to="/admin/crear-usuario">Crear</Link>
+                    </Button>
+                </div>
+            </div>
+
+            <div className="row">
+                
+            </div>
         </div>
     )
 }
